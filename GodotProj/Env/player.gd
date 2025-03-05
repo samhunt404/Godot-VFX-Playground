33bb74aa1d54b8e64286345962c4fb943ad85314
samhunt404 <samuelhunt404@gmail.com>
2025-03-05 08:50:47 -0700

    Finish Portals

--- GodotProj/Env/player.gd
+++ GodotProj/Env/player.gd
@@ -38,6 +38,8 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
+	if(event.is_action_pressed("Quit")):
+		get_tree().call_deferred("quit")
 	if(event is InputEventMouseMotion):
 		rotate_y(-event.relative.x * 0.01 * sensitivity)
 		
--- GodotProj/Portal/Scripts/Behaviors/portal.gd
+++ GodotProj/Portal/Scripts/Behaviors/portal.gd
@@ -15,28 +15,42 @@ func _ready() -> void:
 	viewport = $Renderer
 	
 	viewport.size = get_tree().get_root().size
-	portalVis._update_texture(viewport.get_texture())
 	
 	
 
-
+var init := false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	#null check for when viewport isn't initialized
-	if(get_viewport().get_camera_3d() == null):
-		return
-	var rotDiff = (rotation.y - PI) - paired.rotation.y
-	camControl.transform = paired.global_transform.inverse().rotated(Vector3.UP,rotDiff) * get_viewport().get_camera_3d().global_transform 
+	if(not init and paired != null):
+		paired.portalVis._update_texture(viewport.get_texture())
+		init = true
 	
+	var cam := get_viewport().get_camera_3d()
+	#null check for when viewport isn't initialized
+	if(cam == null):
+		return
+	#check if far enough away to not update the texture to save some performance\
+	if(position.distance_to(cam.position) > 10.0 && paired.position.distance_to(cam.position) > 10.0):
+		return
+	var rotDiff := (paired.global_rotation.y - global_rotation.y)
+	camControl.transform = paired.transform.inverse().rotated(Vector3.UP,rotDiff) * cam.global_transform 
+	#camControl.global_position = to_global(paired.to_local(cam.global_position));
+	#camControl.global_rotation = (global_rotation - paired.global_rotation) + cam.global_rotation
 
 func _teleport_obj(other : Node3D):
 	if(not collisionEnabled):
 		return
 	if(other is CharacterBody3D or other is RigidBody3D):
+		paired.portalVis.visible = false
+		
 		paired.collisionEnabled = false
 		other.global_position = paired.camControl.global_position
-		other.global_position.y -= 1.5 #offset for player height
+		
+		other.position -= get_viewport().get_camera_3d().position
+		
 
 func _reset_teleport(other : Node3D):
 	if(other is CharacterBody3D or other is RigidBody3D):
+		portalVis.visible = true
+		
 		collisionEnabled = true
