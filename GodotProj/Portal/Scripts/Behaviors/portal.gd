b9e8fcb8efb80607de4c776656c7392e8dd26236
samhunt404 <samuelhunt404@gmail.com>
2025-03-05 20:59:23 -0700

    update stuff

--- GodotProj/Portal/Scripts/Behaviors/portal.gd
+++ GodotProj/Portal/Scripts/Behaviors/portal.gd
@@ -7,6 +7,7 @@ extends Node3D
 @onready var portalVis : MeshInstance3D
 @onready var viewport : SubViewport
 
+
 var collisionEnabled = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -32,7 +33,7 @@ func _process(_delta: float) -> void:
 	#check if far enough away to not update the texture to save some performance\
 	if(position.distance_to(cam.position) > 10.0 && paired.position.distance_to(cam.position) > 10.0):
 		return
-	var rotDiff := (paired.global_rotation.y - global_rotation.y)
+	var rotDiff := PI-(paired.global_rotation.y - global_rotation.y)
 	camControl.transform = paired.transform.inverse().rotated(Vector3.UP,rotDiff) * cam.global_transform 
 	#camControl.global_position = to_global(paired.to_local(cam.global_position));
 	#camControl.global_rotation = (global_rotation - paired.global_rotation) + cam.global_rotation
@@ -42,15 +43,15 @@ func _teleport_obj(other : Node3D):
 		return
 	if(other is CharacterBody3D or other is RigidBody3D):
 		paired.portalVis.visible = false
-		
 		paired.collisionEnabled = false
-		other.global_position = paired.camControl.global_position
 		
+		other.global_position = paired.camControl.global_position
+		other.global_rotation.y = paired.camControl.global_rotation.y
 		other.position -= get_viewport().get_camera_3d().position
+		
 		
 
 func _reset_teleport(other : Node3D):
 	if(other is CharacterBody3D or other is RigidBody3D):
 		portalVis.visible = true
-		
 		collisionEnabled = true
